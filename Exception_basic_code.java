4dbd5e88dcae455fa8b26259b94e874314abcd04
dev_nick <developernikhil28@gmail.com>
2025-08-19 21:41:04 +0530

    Added file

---
+++ Exception_basic_code.java
@@ -0,0 +1,24 @@
+// Exceptions are run-time errors
+
+public class Exception_basic_code
+{
+    public static void main(String[] args)
+    {
+
+        int i=0;
+        int j=0;
+
+        try
+        {
+            j=18/i;
+        }
+        catch(Exception e) // "e" is an object of Exception class
+        {
+            // System.out.println("Something went wrong"+ e); // this will print the error as well
+            System.out.println("Something went wrong");
+        }
+        System.out.println(j);
+
+        System.out.println("Bye");
+    }
+}
