3879a5878c2de0f1b4a38d894c2df5fa06a06a88
dev_nick <developernikhil28@gmail.com>
2025-08-19 21:38:15 +0530

    Added file

---
+++ Enums_class.java
@@ -0,0 +1,39 @@
+enum Laptops{
+    //	Mackbook(2000), XPS(2200), Surface(1500), ThinkPad(1800);
+    Mackbook(2000), XPS(2200), Surface, ThinkPad(1800);
+
+    private int price;
+
+    private Laptops()
+    {
+        price=500;
+    }
+
+    private Laptops(int price)
+    {
+        this.price=price;
+    }
+
+    public int getPrice()
+    {
+        return price;
+    }
+    public void setPrice(int price)
+    {
+        this.price=price;
+        System.out.println("in Laptop" + this.name());
+    }
+}
+
+public class Enums_class {
+    public static void main(String[] args) {
+
+//    	Laptop lap=Laptop.Mackbook;
+//    	System.out.println(lap+ " : "+lap.getPrice());
+
+        for(Laptops lap : Laptops.values())
+        {
+            System.out.println(lap+" : "+lap.getPrice());
+        }
+    }
+}
