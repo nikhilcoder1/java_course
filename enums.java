e08084a99725e2f17b05ce8d2ab4bdad25569edb
dev_nick <developernikhil28@gmail.com>
2025-08-19 21:38:02 +0530

    Added file

---
+++ enums.java
@@ -0,0 +1,30 @@
+// Status is a class , and inside them are the objects(named constants)
+
+enum Status
+{
+    Running, Failed, Pending, Success;
+}
+
+public class enums
+{
+    public static void main(String[] args)
+    {
+        int i=5;
+    	Status s= Status.Running;
+    	Status s1= Status.Failed;
+    	Status s2= Status.Success;
+
+    	System.out.println(s);
+    	System.out.println(s.ordinal());
+
+        Status[] ss=Status.values();
+        System.out.println(ss);
+
+        for(Status all:ss)
+        {
+            System.out.println(all);
+            System.out.println(all+" : "+all.ordinal());
+        }
+    }
+}
+
