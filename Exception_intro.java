5900efb7e9bf348220d415e4f9b7fd1576dd1a46
dev_nick <developernikhil28@gmail.com>
2025-08-19 21:41:21 +0530

    Added file

---
+++ Exception_intro.java
@@ -0,0 +1,15 @@
+// Types of error:-
+// 1. Complile - time error (syntactical error)
+// 2. Runtime error -> Exception handling
+// 3. Logical error
+
+
+public class Exception_intro
+{
+    public static void main(String[] args)
+    {
+
+    //  System.out.Println();
+        System.out.println(2+2);
+    }
+}
