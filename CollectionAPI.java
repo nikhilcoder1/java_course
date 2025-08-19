a8e36de70bdf802c5fe2e4bdf733b045eae27c91
dev_nick <developernikhil28@gmail.com>
2025-08-19 21:37:07 +0530

    Added file

---
+++ CollectionAPI.java
@@ -0,0 +1,7 @@
+public class CollectionAPI
+{
+//    Collection API -> concept
+//    Collection -> Interface
+//    Collections -> classe with multiple methods
+//    different type of data structures
+}
