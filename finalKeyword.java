42167de21a2bfa01117a83ea489e8105decae4f7
dev_nick <developernikhil28@gmail.com>
2025-09-02 19:14:47 +0530

    Add final keyword

---
+++ finalKeyword.java
@@ -0,0 +1,53 @@
+//final variable : to make constants
+// final method : to prevent method overriding 
+// final class : to prevent Inheritance
+
+//final class Calc
+//{
+//	public void show()
+//	{
+//		System.out.println("in Calc show");
+//	}
+//	public void add(int a, int b)
+//	{
+//		System.out.println(a+b);
+//	}
+//}
+
+
+class Calculator1
+{
+    public final void show()
+    {
+        System.out.println("By Navin");
+    }
+    public void add(int a, int b)
+    {
+        System.out.println(a+b);
+    }
+}
+
+class AdvCalc extends Calc {
+    public void show() {
+        System.out.println("By John");
+
+    }
+
+
+    public static class finalKeyword {
+        public static void main(String[] args) {
+
+//    	final int num=8;
+//    	num=9;
+//    	System.out.println(num);
+
+//    	Calc obj= new Calc();
+//    	obj.show();
+//    	obj.add(4, 5);
+
+            AdvCalc obj = new AdvCalc();
+            obj.show();
+            obj.add(4, 5);
+        }
+    }
+}
